6e2ac1a8ef855f5c82734d1702775425ca9fee33
shafik720 <shafikrasel5@gmail.com>
2023-02-15 23:42:40 +0600

    working on style

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import logo from './logo.svg';
 import './App.css';
 import Header from './Pages/Header/Header';
+import SideHeader from './Pages/SideHeader/SideHeader';
 
 function App() {
   return (
     <div className="main-div">
       <Header></Header>
+      <SideHeader></SideHeader>
     </div>
   );
 }
---
+++ src/Pages/SideHeader/SideHeader.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './SideHeader.css';
+
+const SideHeader = () => {
+    return (
+        <div className="side-header-div">
+            <h2>Side header here</h2>
+        </div>
+    );
+};
+
+export default SideHeader;

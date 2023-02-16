f23d2a7779913a2a9115e91edd087c29cc5d3824
shafik720 <shafikrasel5@gmail.com>
2023-02-16 12:43:12 +0600

    making notepad component

--- src/Pages/ContentBox/ContentBox.js
+++ src/Pages/ContentBox/ContentBox.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import './ContentBox.css';
+import NotepadContent from './NotepadContent/NotepadContent';
 
 const ContentBox = () => {
     return (
         <div className="content-box">
-            <h2>All Content Here</h2>
+            <NotepadContent></NotepadContent>
         </div>
     );
 };
---
+++ src/Pages/ContentBox/NotepadContent/NotepadContent.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const NotepadContent = () => {
+    return (
+        <div>
+            <h2>Notepad here</h2>
+        </div>
+    );
+};
+
+export default NotepadContent;

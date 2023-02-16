5e751db960d5f2bdb41235e10255d136faf22d40
shafik720 <shafikrasel5@gmail.com>
2023-02-16 12:07:30 +0600

    sidebar styling

--- src/Pages/SideHeader/SideHeader.js
+++ src/Pages/SideHeader/SideHeader.js
@@ -1,10 +1,25 @@
 import React from 'react';
 import './SideHeader.css';
+import noteLogo from '../../utilities/img/notes.png';
+import todoLogo from '../../utilities/img/to-do (1).png';
+import watchLogo from '../../utilities/img/stopwatch.png';
+import currencyLogo from '../../utilities/img/money-exchange.png';
 
 const SideHeader = () => {
     return (
         <div className="side-header-div">
-            <h2>Side header here</h2>
+            <div className="side-header-logo">
+                <img src={noteLogo} alt="" />
+            </div>
+            <div className="side-header-logo">
+                <img src={todoLogo} alt="" />
+            </div>
+            <div className="side-header-logo">
+                <img src={watchLogo} alt="" />
+            </div>
+            <div className="side-header-logo">
+                <img className='currency-logo' src={currencyLogo} alt="" />
+            </div>
         </div>
     );
 };

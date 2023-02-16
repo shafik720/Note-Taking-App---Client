88ece419c48398a639685075b2e7dacb209a3240
shafik720 <shafikrasel5@gmail.com>
2023-02-16 10:40:18 +0600

    working on style

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import logo from './logo.svg';
 import './App.css';
 import Header from './Pages/Header/Header';
 import SideHeader from './Pages/SideHeader/SideHeader';
+import ContentBox from './Pages/ContentBox/ContentBox';
 
 function App() {
   return (
     <div className="main-div">
       <Header></Header>
       <SideHeader></SideHeader>
+      <ContentBox></ContentBox>
     </div>
   );
 }
---
+++ src/Pages/ContentBox/ContentBox.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './ContentBox.css';
+
+const ContentBox = () => {
+    return (
+        <div className="content-box">
+            <h2>All Content Here</h2>
+        </div>
+    );
+};
+
+export default ContentBox;

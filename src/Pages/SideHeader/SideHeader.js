d49e69870ab206dcd0177dd9ee6c4980aabcab85
shafik720 <shafikrasel5@gmail.com>
2023-02-16 12:21:14 +0600

    sidebar styling

--- src/Pages/SideHeader/SideHeader.js
+++ src/Pages/SideHeader/SideHeader.js
@@ -4,13 +4,24 @@ import noteLogo from '../../utilities/img/notes.png';
 import todoLogo from '../../utilities/img/to-do (1).png';
 import watchLogo from '../../utilities/img/stopwatch.png';
 import currencyLogo from '../../utilities/img/money-exchange.png';
+import { Button, OverlayTrigger, Tooltip } from 'react-bootstrap';
 
 const SideHeader = () => {
+    // tooltip to show detail on hover
+    const renderTooltip = props => (
+        <Tooltip {...props}>Notepad</Tooltip>
+    );
     return (
         <div className="side-header-div">
-            <div className="side-header-logo">
+            
+            <OverlayTrigger
+                placement="right"
+                delay={{ show: 50, hide: 100 }}
+                overlay={renderTooltip}>
+                <div className="side-header-logo">
                 <img src={noteLogo} alt="" />
             </div>
+            </OverlayTrigger>
             <div className="side-header-logo">
                 <img src={todoLogo} alt="" />
             </div>

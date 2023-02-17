693ed22fd358694c9f10261033e860c32db9c3ec
shafik720 <shafikrasel5@gmail.com>
2023-02-17 17:44:00 +0600

    adding submenu for clicking on profile icon

--- src/Pages/Header/Header.js
+++ src/Pages/Header/Header.js
@@ -3,11 +3,20 @@ import './Header.css';
 import profileLogo from '../../Utilities/img/profile.png'
 
 const Header = () => {
+    // this function will open a popup menu when user click the profile icon on top right corner
+    const openProfileMenu = () =>{
+        document.querySelector('.profile-submenu').classList.add('active');
+    }
     return (
         <div className="header-one-div">
-            <div className="profile-section">
+            <div onClick={openProfileMenu} draggable className="profile-section">
                 <img src={profileLogo} alt="" />
+
+                <div className="profile-submenu">
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Inventore doloremque officiis assumenda repellat sunt? Aperiam magni sequi deleniti a possimus.</p>
             </div>
+            </div>
+            
         </div>
     );
 };

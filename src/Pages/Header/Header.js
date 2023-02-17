6a1519182d1aee16292beaaa72fb4a424dd78bb8
shafik720 <shafikrasel5@gmail.com>
2023-02-17 19:31:38 +0600

    working on authentication

--- src/Pages/Header/Header.js
+++ src/Pages/Header/Header.js
@@ -1,27 +1,37 @@
 import React from 'react';
 import './Header.css';
 import profileLogo from '../../Utilities/img/profile.png'
+import { useNavigate } from 'react-router';
 
 const Header = () => {
     // this function will open a popup menu when user click the profile icon on top right corner
-    const openProfileMenu = () =>{
+    const openProfileMenu = () => {
         document.querySelector('.profile-submenu').classList.add('active');
     }
-    document.body.addEventListener('click',(e)=>{
-        if(!e.target.classList.contains('profileIcon')){
+    // closing function for profile sub menu 
+    document.body.addEventListener('click', (e) => {
+        if (!e.target.classList.contains('profileIcon')) {
             document.querySelector('.profile-submenu').classList.remove('active');
         }
     })
+
+    const navigate = useNavigate();
+    function jumpToLoginPage(){
+        navigate('/login');
+    }
+    
     return (
         <div className="header-one-div">
             <div onClick={openProfileMenu} draggable className="profile-section">
                 <img className="profileIcon" src={profileLogo} alt="" />
 
+                {/* ------------------------ profile sub menu starts here --------------------------- */}
                 <div className="profile-submenu">
-                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Inventore doloremque officiis assumenda repellat sunt? Aperiam magni sequi deleniti a possimus.</p>
+                    <p> <span onClick={jumpToLoginPage}>Login</span> / <span>Sign-Up</span></p>
+                    
+                </div>
             </div>
-            </div>
-            
+
         </div>
     );
 };
